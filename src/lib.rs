2dfdbac8c98d5fc2eeb21502e28f7e84f1c2ef46
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-06-06 15:48:17 +0200

    add assertions about various types
    Assertions like implementing `Send` and `Sync` and the size of the type.

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,52 @@ mod state;
 
 // TODO: try to relax the ordering, on a per call basis.
 const DEFAULT_ORDERING: Ordering = Ordering::SeqCst;
+
+#[cfg(test)]
+mod assertions {
+    use std::mem;
+
+    use super::pos::AtomicPos;
+    use super::state::AtomicState;
+    use super::segment::{Segment, SegmentData, SegmentId, SEGMENT_SIZE};
+
+    fn assert_send<T: Send>() {}
+    fn assert_sync<T: Sync>() {}
+    fn assert_size<T>(want: usize) {
+        assert_eq!(mem::size_of::<T>(), want);
+    }
+
+    #[test]
+    fn atomic_state() {
+        assert_send::<AtomicState>();
+        assert_sync::<AtomicState>();
+
+        #[cfg(target_pointer_width = "64")]
+        let want = 8;
+        #[cfg(target_pointer_width = "32")]
+        let want = 4;
+        assert_size::<AtomicState>(want);
+    }
+
+    #[test]
+    fn atomic_pos() {
+        assert_send::<AtomicPos>();
+        assert_sync::<AtomicPos>();
+
+        #[cfg(target_pointer_width = "64")]
+        let want = 8;
+        #[cfg(target_pointer_width = "32")]
+        let want = 4;
+        assert_size::<AtomicPos>(want);
+    }
+
+    #[test]
+    fn segment_data() {
+        assert_send::<SegmentData<u64>>();
+        assert_sync::<SegmentData<u64>>();
+
+        // Size of `AtomicState` + 8 bytes for `u64`.
+        let want = mem::size_of::<AtomicState>() + 8;
+        assert_size::<SegmentData<u64>>(want);
+    }
+}
