b69ba8f2d418100423fe961616b06c6c1249be86
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-06-22 18:28:20 +0200

    add assertion tests for `Queue`

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ mod assertions {
 
     use super::state::AtomicState;
     use super::segment::{Segment, SegmentData, SEGMENT_SIZE};
+    use super::queue::Queue;
 
     fn assert_send<T: Send>() {}
     fn assert_sync<T: Sync>() {}
@@ -63,5 +64,19 @@ mod assertions {
         #[cfg(target_pointer_width = "32")]
         let want = 8 + (SEGMENT_SIZE * (8 + 8 + 8)) + 4 + 4;
         assert_size::<Segment<u64>>(want);
+    }
+
+    #[test]
+    fn queue() {
+        assert_send::<Queue<u64>>();
+        assert_sync::<Queue<u64>>();
+        assert_debug::<Queue<u64>>();
+
+        // 2x pointer for atomic pointers, 2x atomic isize.
+        #[cfg(target_pointer_width = "64")]
+        let want = 8 + 8 + 8 + 8;
+        #[cfg(target_pointer_width = "32")]
+        let want = 4 + 4 + 4 + 4;
+        assert_size::<Queue<u64>>(want);
     }
 }
