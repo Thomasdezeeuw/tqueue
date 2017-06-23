42d71a31002da4dbb1b6094cb3fb3cca6661ef2e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-06-23 17:25:39 +0200

    move `Segment` and `Item` assertions into `segment.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -35,34 +35,6 @@ mod assertions {
     }
 
     #[test]
-    fn item() {
-        assert_send::<Item<u64>>();
-        assert_sync::<Item<u64>>();
-        assert_debug::<Item<u64>>();
-
-        // 8 or 4 for the state, 8 bytes for the Option, 8 for u64 (the data).
-        #[cfg(target_pointer_width = "64")]
-        let want = 8 + 8 + 8;
-        #[cfg(target_pointer_width = "32")]
-        let want = 4 + 8 + 8;
-        assert_size::<Item<u64>>(want);
-    }
-
-    #[test]
-    fn segment() {
-        assert_send::<Segment<u64>>();
-        assert_sync::<Segment<u64>>();
-        assert_debug::<Segment<u64>>();
-
-        // 8 or 4 bytes for the id, `SEGMENT_SIZE` * `Item`, 2x pointers.
-        #[cfg(target_pointer_width = "64")]
-        let want = 8 + (SEGMENT_SIZE * (8 + 8 + 8)) + 8 + 8;
-        #[cfg(target_pointer_width = "32")]
-        let want = 4 + (SEGMENT_SIZE * (4 + 8 + 8)) + 4 + 4;
-        assert_size::<Segment<u64>>(want);
-    }
-
-    #[test]
     fn queue() {
         assert_send::<Queue<u64>>();
         assert_sync::<Queue<u64>>();
--- src/segment.rs
+++ src/segment.rs
@@ -630,6 +630,41 @@ mod tests {
 
     use super::*;
 
+    fn assert_send<T: Send>() {}
+    fn assert_sync<T: Sync>() {}
+    fn assert_debug<T: fmt::Debug>() {}
+    fn assert_size<T>(want: usize) {
+        assert_eq!(mem::size_of::<T>(), want);
+    }
+
+    #[test]
+    fn item_assertions() {
+        assert_send::<Item<u64>>();
+        assert_sync::<Item<u64>>();
+        assert_debug::<Item<u64>>();
+
+        // 8 or 4 for the state, 8 bytes for the Option, 8 for u64 (the data).
+        #[cfg(target_pointer_width = "64")]
+        let want = 8 + 8 + 8;
+        #[cfg(target_pointer_width = "32")]
+        let want = 4 + 8 + 8;
+        assert_size::<Item<u64>>(want);
+    }
+
+    #[test]
+    fn segment_assertions() {
+        assert_send::<Segment<u64>>();
+        assert_sync::<Segment<u64>>();
+        assert_debug::<Segment<u64>>();
+
+        // 8 or 4 bytes for the id, `SEGMENT_SIZE` * `Item`, 2x pointers.
+        #[cfg(target_pointer_width = "64")]
+        let want = 8 + (SEGMENT_SIZE * (8 + 8 + 8)) + 8 + 8;
+        #[cfg(target_pointer_width = "32")]
+        let want = 4 + (SEGMENT_SIZE * (4 + 8 + 8)) + 4 + 4;
+        assert_size::<Segment<u64>>(want);
+    }
+
     #[derive(Eq, PartialEq, Ord, PartialOrd, Debug, Clone)]
     struct NoCopy(usize);
 
