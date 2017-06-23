bba2390f04145c7d3a4123ba287141025c28d267
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-06-23 17:29:17 +0200

    move `Queue` assertions into `queue.rs`

--- src/queue.rs
+++ src/queue.rs
@@ -151,3 +151,31 @@ impl<T> fmt::Debug for Queue<T> {
         f.pad("Queue{{ ... }}")
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::mem;
+
+    use super::*;
+
+    fn assert_send<T: Send>() {}
+    fn assert_sync<T: Sync>() {}
+    fn assert_debug<T: fmt::Debug>() {}
+    fn assert_size<T>(want: usize) {
+        assert_eq!(mem::size_of::<T>(), want);
+    }
+
+    #[test]
+    fn queue() {
+        assert_send::<Queue<u64>>();
+        assert_sync::<Queue<u64>>();
+        assert_debug::<Queue<u64>>();
+
+        // 2x pointer for atomic pointers, 2x atomic isize.
+        #[cfg(target_pointer_width = "64")]
+        let want = 8 + 8 + 8 + 8;
+        #[cfg(target_pointer_width = "32")]
+        let want = 4 + 4 + 4 + 4;
+        assert_size::<Queue<u64>>(want);
+    }
+}
