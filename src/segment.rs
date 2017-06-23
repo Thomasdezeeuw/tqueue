ea826001e1da9d5c6ec8c7e87a27fd390cd529f6
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-06-23 17:53:44 +0200

    don't make `SEGMENT_SIZE` public

--- src/segment.rs
+++ src/segment.rs
@@ -13,7 +13,7 @@ const DEFAULT_ORDERING: Ordering = Ordering::SeqCst;
 /// [`Segment`]: struct.Segment.html
 //
 // TODO: benchmark smaller and bigger sizes.
-pub const SEGMENT_SIZE: usize = 32;
+const SEGMENT_SIZE: usize = 32;
 
 /// The id of a [`Segment`].
 ///
