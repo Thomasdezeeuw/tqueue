6ad7ad96769e8eb53863659ccabff4571616bcdd
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-06-24 12:45:26 +0200

    fix `Item` and `Segment` size assertions on i686
    Apparently the pointer is still 8 bytes on i686. Fixes AppVeyor builds
    1.0.21 - 1.0.26

--- src/segment.rs
+++ src/segment.rs
@@ -722,11 +722,8 @@ mod tests {
         assert_sync::<Item<u64>>();
         assert_debug::<Item<u64>>();
 
-        // 8 or 4 for the state, 8 bytes for the Option, 8 for u64 (the data).
-        #[cfg(target_pointer_width = "64")]
+        // 8 for the state, 8 bytes for the Option, 8 for u64 (the data).
         let want = 8 + 8 + 8;
-        #[cfg(target_pointer_width = "32")]
-        let want = 4 + 8 + 8;
         assert_size::<Item<u64>>(want);
     }
 
@@ -736,11 +733,8 @@ mod tests {
         assert_sync::<Segment<u64>>();
         assert_debug::<Segment<u64>>();
 
-        // 8 or 4 bytes for the id, `SEGMENT_SIZE` * `Item`, 2x pointers.
-        #[cfg(target_pointer_width = "64")]
+        // 8 bytes for the id, `SEGMENT_SIZE` * `Item`, 2x pointers.
         let want = 8 + (SEGMENT_SIZE * (8 + 8 + 8)) + 8 + 8;
-        #[cfg(target_pointer_width = "32")]
-        let want = 4 + (SEGMENT_SIZE * (4 + 8 + 8)) + 4 + 4;
         assert_size::<Segment<u64>>(want);
     }
 
